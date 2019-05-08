26a14f09ddea4eac5e2129661f84df133d586b37
Commz <46424175+Commz@users.noreply.github.com>
2019-05-08 16:15:58 +0300

    preparing for float point numbers and deleting all useless functions.

--- gui_calculator_mini/app.py
+++ gui_calculator_mini/app.py
@@ -2,7 +2,6 @@ import tkinter as tk
 
 # version 0.5
 # made with python 3.7.3
-#git
 # initializing variables
 HEIGHT = 550
 WIDTH = 400
@@ -12,73 +11,10 @@ store_num = " "
 op = " "
 final_num = " "
 
-def num0():
+def num(numero):
     global cur_num
     global cur_num2
-    cur_num = cur_num + "0"
-    screen["text"] = cur_num
-    cur_num2 = cur_num
-
-def num1():
-    global cur_num
-    global cur_num2
-    cur_num = cur_num + "1"
-    screen["text"] = cur_num
-    cur_num2 = cur_num
-
-def num2():
-    global cur_num
-    global cur_num2
-    cur_num = cur_num + "2"
-    screen["text"] = cur_num
-    cur_num2 = cur_num
-
-def num3():
-    global cur_num
-    global cur_num2
-    cur_num = cur_num + "3"
-    screen["text"] = cur_num
-    cur_num2 = cur_num
-
-def num4():
-    global cur_num
-    global cur_num2
-    cur_num = cur_num + "4"
-    screen["text"] = cur_num
-    cur_num2 = cur_num
-
-def num5():
-    global cur_num
-    global cur_num2
-    cur_num = cur_num + "5"
-    screen["text"] = cur_num
-    cur_num2 = cur_num
-
-def num6():
-    global cur_num
-    global cur_num2
-    cur_num = cur_num + "6"
-    screen["text"] = cur_num
-    cur_num2 = cur_num
-
-def num7():
-    global cur_num
-    global cur_num2
-    cur_num = cur_num + "7"
-    screen["text"] = cur_num
-    cur_num2 = cur_num
-
-def num8():
-    global cur_num
-    global cur_num2
-    cur_num = cur_num + "8"
-    screen["text"] = cur_num
-    cur_num2 = cur_num
-
-def num9():
-    global cur_num
-    global cur_num2
-    cur_num = cur_num + "9"
+    cur_num = cur_num + numero
     screen["text"] = cur_num
     cur_num2 = cur_num
 
@@ -136,8 +72,8 @@ def final_op():
     global store_num
     global cur_num2
     global final_num
-    int_store_num = int(store_num)
-    int_cur_num2 = int(cur_num2)
+    int_store_num = float(store_num)
+    int_cur_num2 = float(cur_num2)
 
     if op == 1:
         final_num = int_store_num + int_cur_num2
@@ -167,7 +103,7 @@ frame = tk.Frame(root)
 frame.place(relx=0.5, rely=0, relwidth=1, relheight=1, anchor="n")
 # placing the frame for all of the other objects
 
-button0 = tk.Button(frame, text="0", bg="lightgrey", fg="black", font=40, command= num0)
+button0 = tk.Button(frame, text="0", bg="lightgrey", fg="black", font=40, command=lambda: num("0"))
 button0.place(relx=0, rely=0.8, relwidth=0.75, relheight=0.2)
 # button for the number 0
 
@@ -195,39 +131,39 @@ button_Addition = tk.Button(frame, text="+", bg="lightgrey", fg="black", font=40
 button_Addition.place(relx=0.75, rely=0.2, relwidth=0.25, relheight=0.2)
 # button for the addition operation
 
-button1 = tk.Button(frame, text="1", bg="lightgrey", fg="black", font=40, command= num1)
+button1 = tk.Button(frame, text="1", bg="lightgrey", fg="black", font=40, command=lambda: num("1"))
 button1.place(relx=0, rely=0.6, relwidth=0.25, relheight=0.2)
 # button for the number 1
 
-button2 = tk.Button(frame, text="2", bg="lightgrey", fg="black", font=40, command= num2)
+button2 = tk.Button(frame, text="2", bg="lightgrey", fg="black", font=40, command=lambda: num("1"))
 button2.place(relx=0.25, rely=0.6, relwidth=0.25, relheight=0.2)
 #button for the number 2
 
-button3 = tk.Button(frame, text="3", bg="lightgrey", fg="black", font=40, command= num3)
+button3 = tk.Button(frame, text="3", bg="lightgrey", fg="black", font=40, command=lambda: num("3"))
 button3.place(relx=0.50, rely=0.6, relwidth=0.25, relheight=0.2)
 #button for the number 3
 
-button4 = tk.Button(frame, text="4", bg="lightgrey", fg="black", font=40, command= num4)
+button4 = tk.Button(frame, text="4", bg="lightgrey", fg="black", font=40, command=lambda: num("4"))
 button4.place(relx=0, rely=0.4, relwidth=0.25, relheight=0.2)
 #button for the number 4
 
-button5 = tk.Button(frame, text="5", bg="lightgrey", fg="black", font=40, command= num5)
+button5 = tk.Button(frame, text="5", bg="lightgrey", fg="black", font=40, command=lambda: num("5"))
 button5.place(relx=0.25, rely=0.4, relwidth=0.25, relheight=0.2)
 #button for the number 5
 
-button6 = tk.Button(frame, text="6", bg="lightgrey", fg="black", font=40, command= num6)
+button6 = tk.Button(frame, text="6", bg="lightgrey", fg="black", font=40, command=lambda: num("6"))
 button6.place(relx=0.5, rely=0.4, relwidth=0.25, relheight=0.2)
 #button for the number 6
 
-button7 = tk.Button(frame, text="7", bg="lightgrey", fg="black", font=40, command= num7)
+button7 = tk.Button(frame, text="7", bg="lightgrey", fg="black", font=40, command=lambda: num("7"))
 button7.place(relx=0, rely=0.2, relwidth=0.25, relheight=0.2)
 #button for the number 7
 
-button8 = tk.Button(frame, text="8", bg="lightgrey", fg="black", font=40, command= num8)
+button8 = tk.Button(frame, text="8", bg="lightgrey", fg="black", font=40, command=lambda: num("8"))
 button8.place(relx=0.25, rely=0.2, relwidth=0.25, relheight=0.2)
 #button for the number 8
 
-button9 = tk.Button(frame, text="9", bg="lightgrey", fg="black", font=40, command= num9)
+button9 = tk.Button(frame, text="9", bg="lightgrey", fg="black", font=40, command=lambda: num("9"))
 button9.place(relx=0.5, rely=0.2, relwidth=0.25, relheight=0.2)
 #button for the number 9
 
